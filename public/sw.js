c57f067bad51fad3504763b91c6277dcbd3dcba5
HenryMFate <ryanmholzem@gmail.com>
2025-09-19 16:59:36 -0500

    Add files via upload

--- public/sw.js
+++ public/sw.js
@@ -1,22 +1,18 @@
-const CACHE = 'ma-v11';
-
 self.addEventListener('install', (e) => {
-  self.skipWaiting();
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(['/', '/manifest.json'])));
+  e.waitUntil(caches.open('ma-v12').then(cache => cache.addAll(['/','/manifest.json'])));
 });
-
-self.addEventListener('activate', (e) => {
-  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))).then(()=>self.clients.claim());
-});
-
 self.addEventListener('fetch', (e) => {
   e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
 });
-
-self.addEventListener('notificationclick', (event) => {
+self.addEventListener('notificationclick', function(event) {
   event.notification.close();
   event.waitUntil((async () => {
-    const all = await clients.matchAll({ includeUncontrolled: true, type: 'window' });
-    if (all && all.length) { all[0].focus(); } else { clients.openWindow('/'); }
+    const allClients = await clients.matchAll({ includeUncontrolled: true, type: 'window' });
+    if (allClients && allClients.length) {
+      const client = allClients[0];
+      client.focus();
+    } else {
+      clients.openWindow('/');
+    }
   })());
 });
