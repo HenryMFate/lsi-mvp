973732a743d612c39c513102348063baace50083
HenryMFate <ryanmholzem@gmail.com>
2025-09-21 14:30:55 -0500

    Add files via upload

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,9 @@
-import { registerSW } from '../lib/registerSW'
-import { useEffect } from 'react'
 import type { AppProps } from 'next/app'
 import '../styles/globals.css'
+import { useEffect } from 'react'
+import { registerSW } from '../lib/registerSW'
 
-export default function MyApp({
-  const sb = getSupabase(); Component, pageProps }: AppProps){ useEffect(()=>{registerSW()},[]); return <Component {...pageProps} /> }
+export default function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(() => { registerSW(); }, []);
+  return <Component {...pageProps} />
+}
